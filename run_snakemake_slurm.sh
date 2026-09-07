7edca1067a713ee54ea2d3545d9aca3f027d4138
Prashant K <ppresh.ant@gmail.com>
2026-09-07 17:40:36 -0600

    Move #SBATCH directives to the start: job submission directives - Add `slurm-qos` and `slurm-partition` at the top of the slurm config profile

--- run_snakemake_slurm.sh
+++ run_snakemake_slurm.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# SBATCH directives for Slurm job submission of Snakemake workflow
-# Usage: sbatch run_snakemake_slurm.sh
-
-# export path shortcut for the repo and run directories
-export REPO=/home/$USER/deepmut_variant_analysis/gyoza
-export RUN=/scratch/alpine/$USER/deepmut_variant_analysis/gyoza/toy
-
 #SBATCH --job-name=snakemake
 #SBATCH --ntasks=1
 #SBATCH --qos=cpu-normal
@@ -14,8 +7,12 @@ export RUN=/scratch/alpine/$USER/deepmut_variant_analysis/gyoza/toy
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
-#SBATCH --output=$RUN/logs/smk-%j.out
-#SBATCH --error=$RUN/logs/smk-%j.err
+#SBATCH --output=/scratch/alpine/%u/deepmut_variant_analysis/gyoza/toy/logs/smk-%j.out
+#SBATCH --error=/scratch/alpine/%u/deepmut_variant_analysis/gyoza/toy/logs/smk-%j.err
+
+# export path shortcut for the repo and run directories
+export REPO=/home/$USER/deepmut_variant_analysis/gyoza
+export RUN=/scratch/alpine/$USER/deepmut_variant_analysis/gyoza/toy
 
 # Load modules and activate the conda environment
 module load slurm/alpine
@@ -24,6 +21,6 @@ mamba activate snakemake
 
 # Run Snakemake with the specified profile and directory
 snakemake \
-  --profile profiles/slurm/ \
+  --profile "$REPO/profiles/slurm" \
   --directory $RUN \
   --rerun-incomplete
